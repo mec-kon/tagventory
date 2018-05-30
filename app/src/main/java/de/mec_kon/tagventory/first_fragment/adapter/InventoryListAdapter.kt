ad7cb48d79d9d763ff5530762373f61c9375a784
mec-kon <mec-kon@web.de>
2018-05-30 14:50:03 +0200

    tag list fully implemented again

--- app/src/main/java/de/mec_kon/tagventory/first_fragment/adapter/InventoryListAdapter.kt
+++ app/src/main/java/de/mec_kon/tagventory/first_fragment/adapter/InventoryListAdapter.kt
@@ -2,6 +2,7 @@ package de.mec_kon.tagventory.first_fragment.adapter
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.PorterDuff
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -13,7 +14,8 @@ import de.mec_kon.tagventory.first_fragment.datastructure.InventoryItem
 import kotlinx.android.synthetic.main.nav_header_main.view.*
 import kotlinx.android.synthetic.main.single_inventory_list_item.view.*
 
-class InventoryListAdapter(private val items: ArrayList<InventoryItem>, private val context: Activity) : RecyclerView.Adapter<InventoryListAdapter.ViewHolder>() {
+class InventoryListAdapter(private val items: ArrayList<InventoryItem>, private val context: Activity) :
+        RecyclerView.Adapter<InventoryListAdapter.ViewHolder>() {
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -42,17 +44,28 @@ class InventoryListAdapter(private val items: ArrayList<InventoryItem>, private 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bindItems(item: InventoryItem) {
             itemView.inventory_list_name.text = item.name
-            //itemView.inventory_list_tags = item.tagList
 
             ////////// respective tag list //////////
+
+            val inflater = context.layoutInflater
 
             for (i in 0 until item.tagList.size) {
 
                 // add placeholder textview
                 val placeholderTextView = TextView(context)
-                placeholderTextView.setPadding(10,0,10,0)
-                placeholderTextView.text = "TAG"
+                placeholderTextView.setPadding(10, 0, 10, 0)
+
                 itemView.inventory_list_tags.addView(placeholderTextView)
+
+                // add textview
+                val tagView = inflater.inflate(R.layout.inventory_tag_item, null)
+                val xmlTagItem = tagView.findViewById(R.id.tag_item) as TextView
+                xmlTagItem.text = items[position].tagList[i].name
+
+                val roundedTagDesignBG = xmlTagItem.background
+                roundedTagDesignBG.setColorFilter(items[position].tagList[i].color, PorterDuff.Mode.SRC_ATOP)
+
+                itemView.inventory_list_tags.addView(xmlTagItem)
 
             }
         }
